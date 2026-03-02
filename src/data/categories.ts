cee983f3a229eccfa1a3915be025bdf8eda90a12
wisnialeszno89 <wisnialeszno89@gmail.com>
2026-03-02 12:15:14 +0000

    fix: align data structure with alias config

---
+++ src/data/categories.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    slug: "psycholog",
+    name: "Psycholog",
+  },
+  {
+    slug: "prawo",
+    name: "Prawo rodzinne",
+  },
+  {
+    slug: "trening",
+    name: "Trening",
+  },
+  {
+    slug: "wyjazdy",
+    name: "Wyjazdy rozwojowe",
+  },
+];
