8bcd15481a25478ff37b5ff45e4bf1f178fc9a48
wisnialeszno89 <wisnialeszno89@gmail.com>
2026-03-17 14:20:20 +0000

    remove user state bar (premature personalization)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import HomeArticles from "@/features/home/HomeArticles"
 import TrustSection from "@/features/home/TrustSection"
 import FeaturedPartners from "@/features/marketplace/FeaturedPartners"
 import NaviMindButton from "@/features/navimind/NaviMindButton"
-import UserStateBar from "@/features/home/UserStateBar"
 
 export default function Home() {
 
@@ -17,15 +16,11 @@ export default function Home() {
         title="MenMind"
         intro="Miejsce dla mężczyzn, którzy chcą odzyskać kierunek, spokój i sprawczość."
       />
-      
-            {/* 🔥 1. ŚWIATY */}
-            <UserStateBar />
+
       <WorldPreview />
 
-      {/* 🔥 2. TEST (główne wejście jeśli ktoś nie wie) */}
       <StartSection />
 
-      {/* 🔥 3. NAVIMIND (backup emocjonalny) */}
       <section className="py-20 border-t">
 
         <div className="max-w-4xl mx-auto px-6 text-center">
@@ -46,15 +41,11 @@ export default function Home() {
 
       </section>
 
-      {/* 🔥 4. TREŚCI */}
-      
       <HomeArticles />
 
       <TrustSection />
 
       <FeaturedPartners />
-
-      
 
     </main>
 
