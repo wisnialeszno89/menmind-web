31e28a1ab6b1c546ee322f6950c9af2b2c81b709
wisnialeszno89 <wisnialeszno89@gmail.com>
2026-04-02 08:50:34 +0000

    fix world type casting for SEO articles

--- src/content/index.ts
+++ src/content/index.ts
@@ -4,6 +4,7 @@ import { kryzys } from "./kryzys"
 import { odbudowa } from "./odbudowa"
 import { wzrost } from "./wzrost"
 import { ojcostwo } from "./ojcostwo"
+import { articles as seoArticles } from "./articles"
 
 export const worlds = {
   kryzys,
@@ -14,9 +15,20 @@ export const worlds = {
 
 export type WorldKey = keyof typeof worlds
 
+const seoMapped: Article[] = seoArticles.map(a => ({
+  slug: a.slug,
+  title: a.title,
+  description: a.intro,
+  world: a.world as Article["world"],
+  category: "seo",
+  keywords: [],
+  content: ""
+}))
+
 export const allArticles: Article[] = [
   ...kryzys,
   ...odbudowa,
   ...wzrost,
-  ...ojcostwo
+  ...ojcostwo,
+  ...seoMapped
 ]
